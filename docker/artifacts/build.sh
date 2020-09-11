9ab4fe13cd72b3045bcb1582a9ef362d16f43727
Nadun De Silva <nadunrds@gmail.com>
2020-09-11 10:13:03 +0530

    Remove Performance Common Repository

--- docker/artifacts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-DEV_BALLERINA_CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-pushd "${DEV_BALLERINA_CURRENT_SCRIPT_DIR}/repos/wso2/performance-common"
-mvn clean install -Dmaven.test.skip=true
-popd
